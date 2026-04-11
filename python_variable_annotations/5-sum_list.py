06b039712ae216a0d45a248f2f114331dba43d46
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-04-11 15:17:36 +0400

    Delete python_variable_annotations directory

--- python_variable_annotations/5-sum_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Define a typed list summation helper."""
-
-from typing import List
-
-
-def sum_list(input_list: List[float]) -> float:
-    """Return the sum of a list of floating-point numbers."""
-    return sum(input_list)
