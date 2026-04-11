a758cea71f8c44c296f4be6c59ef5656351853a2
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-04-11 15:10:58 +0400

    Delete python_variable_annotations directory

--- python_variable_annotations/2-floor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Define a typed floor helper."""
-
-import math
-
-
-def floor(n: float) -> int:
-    """Return the floor of a floating-point number."""
-    return math.floor(n)
